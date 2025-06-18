4f2459491adea73eddb77fb93838f27631912653
Artur Kharin <kharin.art@gmail.com>
2025-06-18 14:06:53 +0300

    Multiple changes to installers to publish in WinGet. (#37)
    * Update release.yml (#29)
    
    * Update release.yml
    
    * Update Main.yml to run PR on develop
    
    * Enabled dependabot (#30)
    
    * Update dependabot.yml
    
    * Update dependencies. (#32)
    
    * Allow to set install folder from command line (InstallFolder="<Path>") (#33)
    
    * Updated copyrights for consistency. (#34)
    
    * Updated copyrights for consistency.
    
    * Updatred cargo.toml encoding
    
    * Update release.yml (#35)
    
    * Update release.yml prerelease. (#36)

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyFileVersion("0.0.0")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Processes Priority Manager")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyCopyright("Copyright Artur Kharin©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatform("windows10.0.17763")]
 
